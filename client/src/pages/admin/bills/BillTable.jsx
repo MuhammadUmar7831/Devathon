77b0ce8d7f131085c4fc6addf70469acff5cedae
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-09-07 12:16:06 +0500

    Added Search Bill Page

--- client/src/pages/admin/bills/BillTable.jsx
+++ client/src/pages/admin/bills/BillTable.jsx
@@ -1,7 +1,80 @@
 import React from "react"
 
 const BillTable = () => {
-  return <></>
+  return (
+    <>
+      {" "}
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-left rtl:text-right">
+          <thead class="text-white bg-gray-900">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                ID
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Resident
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Month
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Units
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Due Date
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Total
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Status
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-white border-b dark:border-gray-700 ">
+              <th
+                scope="row"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+              >
+                1
+              </th>
+              <td class="px-6 py-4">Huzaifa Rizwan</td>
+              <td class="px-6 py-4">September</td>
+              <td class="px-6 py-4">100</td>
+              <td class="px-6 py-4">07/10/24</td>
+              <td class="px-6 py-4">Rs. 10000</td>
+              <td class="px-6 py-4">Unpaid</td>
+
+              <td class="px-6 py-4">
+                <a class="font-medium text-blue-600 hover:underline cursor-pointer">
+                  Download
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <nav
+          class="px-4 text-base h-12 flex items-center flex-column flex-wrap md:flex-row justify-between py-1"
+          aria-label="Table navigation"
+        >
+          <span class=" font-normal text-gray-500 mb-4 md:mb-0 block w-full md:inline md:w-auto">
+            Showing <span class="font-semibold text-gray-900">10</span> records{" "}
+          </span>
+        </nav>
+
+        {false && (
+          <div className="text-center text-sm px-6 py-10 font-medium text-gray-900 whitespace-nowrap">
+            <h2>There are no records to display</h2>
+          </div>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default BillTable
--- client/src/pages/admin/bills/Bills.jsx
+++ client/src/pages/admin/bills/Bills.jsx
@@ -1,17 +1,31 @@
 import React from "react"
+import BillTable from "./BillTable"
 
 const Bills = () => {
   return (
     <>
-      <div class="w-full p-8 flex flex-col gap-16">
+      <div class="w-full p-8 flex flex-col gap-10">
         <div className="flex items-center justify-between">
           <div>
             <h6 className="text-xl">
               <b>Search Bills</b>
             </h6>
           </div>
+          <div className="flex gap-2">
+            <input
+              type="text"
+              class="rounded-lg block p-2 border border-gray-900 text-gray-900"
+              placeholder="Search"
+              required
+            />
+            <button className="bg-gray-900 hover:bg-gray-800 rounded-md text-white p-2 disabled:cursor-not-allowed">
+              Search
+            </button>
+          </div>
         </div>
-        <div></div>
+        <div className="">
+          <BillTable />
+        </div>
       </div>
     </>
   )
