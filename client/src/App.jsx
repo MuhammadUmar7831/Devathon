ec93696e7a85c74059a3fe354bfc245e2871d6c9
Huzaifa Rizwan <huzaifa.rizwan1231@gmail.com>
2024-09-07 11:08:29 +0500

    redux setup completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom"
 import Login from "./pages/auth/Login"
 import Signup from "./pages/auth/Signup"
+import PrivateRoutes from "./components/PrivateRoutes"
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
           <Route path="/" element={<Navigate to="/auth/login" />} />
           <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/signup" element={<Signup />} />
+
+          <Route element={<PrivateRoutes role="admin" />}></Route>
+          <Route element={<PrivateRoutes role="resident" />}></Route>
+          <Route element={<PrivateRoutes role="customerSupport" />}></Route>
         </Routes>
       </Router>
     </>
