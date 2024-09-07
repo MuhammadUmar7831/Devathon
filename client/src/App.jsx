b8d7b9fd99bab57d65ce0b639aa1f172892c541d
MuhammadUmar7831 <mu8494759@gmail.com>
2024-09-07 11:30:23 +0500

    Sidebar Fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import ManageAccounts from "./pages/admin/manageAccounts/ManageAccounts"
 import AddCustomerSupport from "./pages/admin/addCustomerSupport/AddCustomerSupport"
 import Bills from "./pages/admin/bills/Bills"
 import ViewBills from "./pages/resident/viewBills/ViewBills"
+import Profile from "./pages/resident/Profile"
 
 function App() {
   return (
